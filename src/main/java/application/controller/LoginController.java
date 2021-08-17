d0c689545166b9f474d6a569d3ba4160177f8a8b
Nastia Herych <nastia.herych@gmail.com>
2021-08-17 22:35:22 +0300

    clean unnecessary code

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -1,28 +1,20 @@
 package application.controller;
 
-import application.models.User;
 import application.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
     private final UserRepository userRepository;
 
-    public LoginController(UserRepository userRepository){
+    public LoginController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @GetMapping("/login")
-    public String goLogin(){
+    public String goLogin() {
         return "login";
     }
-
-//    @PostMapping("/login")
-//    public String postLogin(User model) {
-//        return "mainPage";
-//    }
 }
--- src/main/java/application/security/CustomUserDetailsService.java
+++ src/main/java/application/security/CustomUserDetailsService.java
@@ -1,13 +1,11 @@
 package application.security;
 
-import application.models.User;
 import application.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
