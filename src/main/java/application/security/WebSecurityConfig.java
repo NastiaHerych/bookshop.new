e27761899329f89d2df7c2d62c666ef1d2b4794b
NastiaHerych <nastia.herych@gmail.com>
2021-11-25 23:35:18 +0200

    login

--- src/main/java/application/security/WebSecurityConfig.java
+++ src/main/java/application/security/WebSecurityConfig.java
@@ -26,8 +26,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/welcome/**", "/register/**").permitAll()
-                .anyRequest().authenticated()
+                 .antMatchers("/mainPage/**").authenticated()
+                .anyRequest().permitAll()
+
                 .and()
                 .formLogin()
                 .loginPage("/login")
