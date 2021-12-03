bd635e46b79c14e051ee5fa3d259ca782852e18b
NastiaHerych <nastia.herych@gmail.com>
2021-12-03 23:21:46 +0200

    new book/lists

--- src/main/java/application/controller/AdminController.java
+++ src/main/java/application/controller/AdminController.java
@@ -1,12 +1,18 @@
 package application.controller;
 
 import application.models.Book;
+import application.models.User;
 import application.repository.BookRepository;
+import application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @PreAuthorize("hasAnyAuthority('ADMIN')")
@@ -14,14 +20,19 @@ public class AdminController {
 
     private final BookRepository bookRepository;
 
-    public AdminController(BookRepository bookRepository){
+    private final UserRepository userRepository;
+
+    public AdminController(BookRepository bookRepository, UserRepository userRepository){
         this.bookRepository = bookRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/adminPage")
     public String goRegister(Model model) {
         model.addAttribute("book", new Book());
         model.addAttribute("message", "HERE");
+        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("books", bookRepository.findAll());
         return "adminPage";
     }
 
@@ -30,4 +41,6 @@ public class AdminController {
         bookRepository.save(book);
         return "adminPage";
     }
+
+
 }
